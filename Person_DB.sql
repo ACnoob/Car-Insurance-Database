dd400a4ca659419ab8c3b29bf2e6037601fe6e94
ACnoob <118096469+ACnoob@users.noreply.github.com>
2022-12-16 19:11:57 +0530

    Upload : Person Database

---
+++ Person_DB.sql
@@ -0,0 +1,61 @@
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('M49415063','Patrick Cleaver','1701 N.Campbell Rd, Dallas, TX-75243');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('R16359565','Courtney Rollins','1530 S.Campbell Rd');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('A16505109','Glenn Tucker','101 Meritline drive');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('C12846987','Ron Harper','43 Greenville Road');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('J32569641','Manoj Punwani','43 Greenville Road');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('U89690723','Shreyash Wasnik','Jagrut Nagar, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('K23298127','Aaradhya Chaple','Jai Mata Nagar, Dighori');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('H12486417','Nishank Jain','Itwari, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('F45677879','Yash Chainani','RCOEM Boys Hostel, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('T56347672','Pravesh Fating','Hanuman Nagar, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('P45879449','Sanskruti Wagh','Bajaj Nagar, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('K65753243','Madhumita Mahapatra','RCOEM Girls Hostel, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('D46986428','Vinay Dhawad','RCOEM Boys Hostel, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('V78892362','Tejas Saraf','RCOEM Boys Hostel, Nagpur');
+INSERT INTO PERSON
+(Driver_id,Full_name,Address)
+VALUES
+('R12098127','Nehul Shrimankar','Civil Lines, Nagpur');
+SELECT * FROM PERSON
